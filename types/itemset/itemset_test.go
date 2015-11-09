5c18851f8dfcd213817e06d75572b8fbec42177a
Tim Henderson <tim.tadh@gmail.com>
2015-11-09 16:57:04 -0500

    All dependencies for testify and test file created
    I decided this time to try out a testing library for use. I required 2
    more dependencies (argh).
    
    1. testify -- the library in use. a BSD 0-clause license.
    2. go-spew -- a BSD 0-clause license.
    3. go-difflib -- a BSD 3-clause license.
    
    So their should be no problem linking them into sfp especially as they
    are testing only dependencies.
    
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- types/itemset/itemset_test.go
+++ types/itemset/itemset_test.go
@@ -1,1 +1,10 @@
 package itemset
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestHello(x *testing.T) {
+	t := assert.New(x)
+	t.Equal("hello", string([]byte("hello")), "wizard %v", 1)
+}
+
