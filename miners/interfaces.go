9b57707423233285feffcd13aae9c522625c761c
Tim Henderson <tim.tadh@gmail.com>
2015-11-30 16:27:56 -0500

    Documented contract of the Miner.Close() function.
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- miners/interfaces.go
+++ miners/interfaces.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-
+// Note: the miner's Close function should close both reporter and the datatype that were passed into it.
 type Miner interface {
 	Mine(lattice.DataType, Reporter) error
 	Close() error
