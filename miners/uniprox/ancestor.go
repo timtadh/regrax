1643a4b0672176c610821241dcd501129fd89040
Tim Henderson <tim.tadh@gmail.com>
2016-01-06 15:51:05 -0500

    a few improvements to ancestor
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- miners/uniprox/ancestor.go
+++ miners/uniprox/ancestor.go
@@ -1,6 +1,7 @@
 package uniprox
 
 import (
+	"log"
 	"math"
 )
 
@@ -51,9 +52,11 @@ func itemsetCommonAncestor(patterns []lattice.Pattern) (_ lattice.Pattern, err e
 }
 
 func graphCommonAncestor(patterns []lattice.Pattern) (lattice.Pattern, error) {
+
+	// construct a in memory configuration for finding common subgraphs of all patterns
 	conf := &config.Config{
 		Support: len(patterns),
-		Samples: 10,
+		Samples: 5,
 	}
 	wlkr := fastmax.NewWalker(conf)
 	wlkr.Reject = false
@@ -61,15 +64,34 @@ func graphCommonAncestor(patterns []lattice.Pattern) (lattice.Pattern, error) {
 	collector := &reporters.Collector{make([]lattice.Node, 0, 10)}
 	rptr := &reporters.Chain{[]miners.Reporter{&reporters.Log{}, reporters.NewUnique(collector)}}
 
+	// closing the walker releases the memory
+	defer func() {
+		err := wlkr.Close()
+		if err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	maxE := int(math.MaxInt32)
+	maxV := int(math.MaxInt32)
+	for _, pat := range patterns {
+		sg := pat.(*graph.Pattern).Sg
+		if len(sg.E) < maxE {
+			maxE = len(sg.E)
+		}
+		if len(sg.V) < maxV {
+			maxV = len(sg.V)
+		}
+	}
+
+	// construct the graph from the patterns
 	Graph := goiso.NewGraph(10, 10)
 	G := &Graph
-	l, err := graph.NewVegLoader(conf, 0, int(math.MaxInt32), 0, int(math.MaxInt32))
+	l, err := graph.NewVegLoader(conf, 0, maxE, 0, maxV)
 	if err != nil {
 		return nil, err
 	}
 	v := l.(*graph.VegLoader)
-
-
 	offset := 0
 	for _, pat := range patterns {
 		sg := pat.(*graph.Pattern).Sg
@@ -82,21 +104,23 @@ func graphCommonAncestor(patterns []lattice.Pattern) (lattice.Pattern, error) {
 		offset += len(sg.V)
 	}
 
+	// compute the starting points (we are now ready to mine)
 	start, err := v.ComputeStartingPoints(G)
 	if err != nil {
 		return nil, err
 	}
 	v.G.FrequentVertices = start
-
 	var dt lattice.DataType = v.G
 
-	errors.Logf("DEBUG", "patterns %v", G)
+	errors.Logf("DEBUG", "patterns %v %v", len(patterns), G)
 
+	// mine
 	err = wlkr.Mine(dt, rptr)
 	if err != nil {
 		return nil, err
 	}
 
+	// extract the largest common subgraph
 	maxLevel := collector.Nodes[0].Pattern().Level()
 	maxPattern := collector.Nodes[0].Pattern()
 	for _, n := range collector.Nodes {
@@ -108,10 +132,6 @@ func graphCommonAncestor(patterns []lattice.Pattern) (lattice.Pattern, error) {
 	}
 	errors.Logf("DEBUG", "ancestor %v", maxPattern)
 
-	err = wlkr.Close()
-	if err != nil {
-		return nil, err
-	}
 
 	return maxPattern, nil
 }
