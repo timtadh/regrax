90e4c96db25beeec38de2581d3edf06f300306da
Tim Henderson <tim.tadh@gmail.com>
2016-03-01 16:36:29 -0500

    Moved sortable []*goiso.SubGraphs to children.go
    It was the only place it was used. Makes since to localize it there.
    
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- types/digraph/emblist_node.go
+++ types/digraph/emblist_node.go
@@ -1,13 +1,10 @@
 package digraph
 
 import (
-	"bytes"
 	"fmt"
-	"sort"
 )
 
 import (
-	"github.com/timtadh/data-structures/errors"
 	"github.com/timtadh/goiso"
 )
 
@@ -23,63 +20,6 @@ type EmbListNode struct {
 type Embedding struct {
 	sg *goiso.SubGraph
 }
-
-type SubGraphs []*goiso.SubGraph
-
-func (sgs SubGraphs) Len() int {
-	return len(sgs)
-}
-
-func (sgs SubGraphs) Less(i, j int) bool {
-	return bytes.Compare(sgs[i].ShortLabel(), sgs[j].ShortLabel()) < 0
-}
-
-func (sgs SubGraphs) Swap(i, j int) {
-	sgs[i], sgs[j] = sgs[j], sgs[i]
-}
-
-func (sgs SubGraphs) Verify() error {
-	if len(sgs) <= 0 {
-		return errors.Errorf("empty partition")
-	}
-	label := sgs[0].ShortLabel()
-	for _, sg := range sgs {
-		if !bytes.Equal(label, sg.ShortLabel()) {
-			return errors.Errorf("bad partition %v %v", sgs[0].Label(), sg.Label())
-		}
-	}
-	return nil
-}
-
-func (sgs SubGraphs) Partition() []SubGraphs {
-	sort.Sort(sgs)
-	parts := make([]SubGraphs, 0, 10)
-	add := func(parts []SubGraphs, buf SubGraphs) []SubGraphs {
-		err := buf.Verify()
-		if err != nil {
-			errors.Logf("ERROR", "%v", err)
-		} else {
-			parts = append(parts, buf)
-		}
-		return parts
-	}
-	buf := make(SubGraphs, 0, 10)
-	var ckey []byte = nil
-	for _, sg := range sgs {
-		label := sg.ShortLabel()
-		if ckey != nil && !bytes.Equal(ckey, label) {
-			parts = add(parts, buf)
-			buf = make(SubGraphs, 0, 10)
-		}
-		ckey = label
-		buf = append(buf, sg)
-	}
-	if len(buf) > 0 {
-		parts = add(parts, buf)
-	}
-	return parts
-}
-
 func NewEmbListNode(Dt *Digraph, sgs SubGraphs) *EmbListNode {
 	if len(sgs) > 0 {
 		return &EmbListNode{newSearchNode(Dt, sgs[0]), sgs}
