73f38d356b321d3a32ab7943b579596da4ba3494
Tim Henderson <tim.tadh@gmail.com>
2016-03-03 14:11:03 -0500

    reduced indentation in MaxTxSupported
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- types/digraph/support.go
+++ types/digraph/support.go
@@ -25,47 +25,46 @@ func MakeTxSupported(attrName string) Supported {
 		supported := make(SubGraphs, 0, len(sgs))
 		seen := set.NewSortedSet(len(sgs))
 		for _, sg := range sgs {
-			if len(sg.V) > 0 {
-				vid := int32(sg.V[0].Id)
-				err := dt.NodeAttrs.DoFind(vid, func(_ int32, attrs map[string]interface{}) error {
-					if attr, has := attrs[attrName]; !has {
-						return errors.Errorf("subgraph %v vertex %v did not have attr %v in attrs %v", sg, sg.V[0], attrName, attrs)
+			if len(sg.V) <= 0 {
+				supported = append(supported, sg)
+				continue
+			}
+			vid := int32(sg.V[0].Id)
+			err := dt.NodeAttrs.DoFind(vid, func(_ int32, attrs map[string]interface{}) error {
+				attr, has := attrs[attrName];
+				if !has {
+					return errors.Errorf("subgraph %v vertex %v did not have attr %v in attrs %v", sg, sg.V[0], attrName, attrs)
+				}
+				switch a := attr.(type) {
+				case json.Number:
+					if n, err := a.Int64(); err != nil {
+						return errors.Errorf("type float is not suppported as an attr value for tx id %v %v for %v", attrName, attr, sg)
 					} else {
-						switch a := attr.(type) {
-						case json.Number:
-							if n, err := a.Int64(); err != nil {
-								return errors.Errorf("type float is not support as an attr value for tx id %v %v for %v", attrName, attr, sg)
-							} else {
-								if !seen.Has(types.Int64(n)) {
-									seen.Add(types.Int64(n))
-									supported = append(supported, sg)
-								}
-							}
-						case map[string]interface{}:
-							return errors.Errorf("type map[string]interface{} is not support as an attr value for tx id")
-						case []interface{}:
-							return errors.Errorf("type []interface{} is not support as an attr value for tx id")
-						case bool:
-							return errors.Errorf("type bool is not support as an attr value for tx id")
-						case nil:
-							return errors.Errorf("type nil is not support as an attr value for tx id")
-						case string:
-							if !seen.Has(types.String(a)) {
-								seen.Add(types.String(a))
-								supported = append(supported, sg)
-							}
-						default:
-							return errors.Errorf("unexpected type %T for %v in %v", attr, attr, sg)
+						if !seen.Has(types.Int64(n)) {
+							seen.Add(types.Int64(n))
+							supported = append(supported, sg)
 						}
 					}
-					return nil
-				})
-				if err != nil {
-					return nil, err
+				case map[string]interface{}:
+					return errors.Errorf("type map[string]interface{} is not suppported as an attr value for tx id")
+				case []interface{}:
+					return errors.Errorf("type []interface{} is not suppported as an attr value for tx id")
+				case bool:
+					return errors.Errorf("type bool is not suppported as an attr value for tx id")
+				case nil:
+					return errors.Errorf("type nil is not suppported as an attr value for tx id")
+				case string:
+					if !seen.Has(types.String(a)) {
+						seen.Add(types.String(a))
+						supported = append(supported, sg)
+					}
+				default:
+					return errors.Errorf("unexpected type %T for %v in %v", attr, attr, sg)
 				}
-			} else {
-				// for empty sg all are supported
-				supported = append(supported, sg)
+				return nil
+			})
+			if err != nil {
+				return nil, err
 			}
 		}
 		return supported, nil
