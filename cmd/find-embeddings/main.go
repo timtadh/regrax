4d23159173b3092ecb4c9a237ccdf02e2c14f01d
Tim Henderson <tim.tadh@gmail.com>
2016-06-13 16:37:55 -0400

    confidence interval for coverage
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- cmd/find-embeddings/main.go
+++ cmd/find-embeddings/main.go
@@ -53,13 +53,15 @@ find-embeddings -p <pattern> <graph>
 }
 
 
-func loadProbabilities(path string) (prs []float64, patterns []string, err error) {
+func loadProbabilities(path string) (prs []float64, patterns []string, count int, err error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, 0, err
 	}
 	prs = make([]float64, 0, 10)
 	patterns = make([]string, 0, 10)
+	seen := make(map[string]bool)
+	count = 0
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		line := strings.TrimSpace(s.Text())
@@ -69,14 +71,42 @@ func loadProbabilities(path string) (prs []float64, patterns []string, err error
 		fields := strings.SplitN(line, ",", 2)
 		pr, err := strconv.ParseFloat(fields[0], 64)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, 0, err
 		}
 		pattern := strings.TrimSpace(fields[1])
-		prs = append(prs, pr)
-		patterns = append(patterns, pattern)
+		count++
+		if _, has := seen[pattern]; !has {
+			seen[pattern] = true
+			prs = append(prs, pr)
+			patterns = append(patterns, pattern)
+		}
 	}
-	return prs, patterns, nil
+	return prs, patterns, count, nil
 }
+
+func loadNames(path string) (prs []float64, patterns []string, count int, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+	prs = make([]float64, 0, 10)
+	patterns = make([]string, 0, 10)
+	seen := make(map[string]bool)
+	count = 0
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := strings.TrimSpace(s.Text())
+		pattern := line
+		count++
+		if _, has := seen[pattern]; !has {
+			seen[pattern] = true
+			patterns = append(patterns, pattern)
+		}
+	}
+	return prs, patterns, count, nil
+}
+
+
 
 
 func main() {
@@ -94,6 +124,7 @@ func run() int {
 			"visualize=",
 			"probabilities=",
 			"samples=",
+			"names=",
 		},
 	)
 	if err != nil {
@@ -104,6 +135,7 @@ func run() int {
 	visual := ""
 	patterns := make([]string, 0, 10)
 	prPath := ""
+	namesPath := ""
 	cpuProfile := ""
 	samples := -1
 	for _, oa := range optargs {
@@ -114,6 +146,8 @@ func run() int {
 			patterns = append(patterns, oa.Arg())
 		case "--probabilities":
 			prPath = cmd.AssertFileExists(oa.Arg())
+		case "--names":
+			namesPath = cmd.AssertFileExists(oa.Arg())
 		case "--samples":
 			samples = cmd.ParseInt(oa.Arg())
 		case "--cpu-profile":
@@ -126,15 +160,29 @@ func run() int {
 		}
 	}
 
-	if prPath != "" && len(patterns) > 0 {
+	if (prPath != "" || namesPath != "") && len(patterns) > 0 {
 		fmt.Fprintf(os.Stderr, "You cannot supply patterns with both (-p) and (--probabilities)\n")
 		cmd.Usage(cmd.ErrorCodes["opts"])
 	}
 
+	if len(patterns) == 0 && prPath == "" && namesPath == "" {
+		fmt.Fprintf(os.Stderr, "You must supply a pattern (-p, --names, --probabilities)\n")
+		cmd.Usage(cmd.ErrorCodes["opts"])
+	}
+
+	var patternCount int = len(patterns)
 	var prs []float64 = nil
 	if prPath != "" {
 		var err error
-		prs, patterns, err = loadProbabilities(prPath)
+		prs, patterns, patternCount, err = loadProbabilities(prPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "There was error loading the probability file\n")
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			return 1
+		}
+	} else if namesPath != "" {
+		var err error
+		prs, patterns, patternCount, err = loadNames(namesPath)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "There was error loading the probability file\n")
 			fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -142,22 +190,9 @@ func run() int {
 		}
 	}
 
-	if len(prs) != 0 && samples == -1 {
-		fmt.Fprintf(os.Stderr, "You must supply the total number samples (with replacement) (--samples) when using (--probabilities)\n")
-		cmd.Usage(cmd.ErrorCodes["opts"])
-	}
-
-	if len(patterns) == 0 {
-		fmt.Fprintf(os.Stderr, "You must supply a pattern (-p)\n")
-		cmd.Usage(cmd.ErrorCodes["opts"])
-	}
-
-	if len(prs) == 0 {
-		prs = make([]float64, 0, len(patterns))
-		p := 1.0/float64(len(patterns))
-		for _ = range patterns {
-			prs = append(prs, p)
-		}
+	if samples < len(prs) {
+		errors.Errorf("INFO", "assuming # of samples is the total number of patterns supplied: %v", patternCount)
+		samples = patternCount
 	}
 
 	conf := &config.Config{}
@@ -184,7 +219,9 @@ func run() int {
 
 	matches := make([]float64, 0, len(patterns))
 	matched := make([]*subgraph.SubGraph, 0, len(patterns))
+	sgEdges := make([]float64, 0, len(patterns))
 	total := 0.0
+	totalEdges := 0.0
 	for i, pattern := range patterns {
 		sg, err := subgraph.ParsePretty(pattern, &graph.G.Colors, graph.G.Labels)
 		if err != nil {
@@ -193,14 +230,17 @@ func run() int {
 			return 1
 		}
 		match, csg, err := sg.EstimateMatch(graph.Indices)
+		match = match * float64(len(sg.E))
 		if err != nil {
 			errors.Logf("ERROR", "%v", err)
 			return 1
 		}
 		matches = append(matches, match)
 		matched = append(matched, csg)
+		sgEdges = append(sgEdges, float64(len(sg.E)))
 		fmt.Printf("%v, %v, %v\n", i+1, match, pattern)
 		total += match
+		totalEdges += float64(len(sg.E))
 		if visualize != nil {
 			dotty, err := csg.VisualizeEmbedding(sg.AsIndices(graph.Indices))
 			if err != nil {
@@ -211,31 +251,89 @@ func run() int {
 			fmt.Fprintln(visualize, dotty)
 		}
 	}
-	fmt.Printf(", %v, sample total of frequent pattern coverage\n", total)
-	fmt.Printf(", %v, sample mean of frequent pattern coverage\n", total/float64(len(patterns)))
-	estMean, estVarMean := estPopMean(samples, prs, matches)
-	fmt.Printf(", %v, estimated population mean\n", estMean)
-	fmt.Printf(", %v, estimated variance population mean\n", estVarMean)
-	fmt.Printf(", %v, estimated standard dev population mean\n", math.Sqrt(estVarMean))
+	errors.Logf("DEBUG", "prs %v",  sum(prs))
+	fmt.Printf(", %v, sample total covered edges\n", total)
+	fmt.Printf(", %v, sample total edges\n", totalEdges)
+	fmt.Printf(", %v, sample covered/total\n", total/totalEdges)
+	fmt.Printf(", %v, sample avg covered\n", total/float64(len(patterns)))
+	fmt.Printf(", %v, sample avg edges\n", totalEdges/float64(len(patterns)))
+
+	if len(prs) > 0 {
+		pis := samplingPrs(samples, prs)
+		jpis := jointSamplingPrs(samples, prs, pis)
+		estN := estPopSize(pis)
+		estTotalMatch := estPopTotal(pis, matches)
+		estVarTotalMatch := estVarTotal(pis, jpis, matches)
+		estTotalEdges := estPopTotal(pis, sgEdges)
+		estVarTotalEdges := estVarTotal(pis, jpis,  sgEdges)
+
+		fmt.Printf("\n")
+		fmt.Printf(", %v, estimated population total of matched edges\n", estTotalMatch)
+		fmt.Printf(", %v, estimated population total of total edges\n", estTotalEdges)
+		fmt.Printf(", %v, estimated var population total of match edges\n", estVarTotalMatch)
+		fmt.Printf(", %v, estimated var population total of total edges\n", estVarTotalEdges)
+		fmt.Printf(", %v, estimated std population total of match edges\n", math.Sqrt(estVarTotalMatch))
+		fmt.Printf(", %v, estimated std population total of total edges\n", math.Sqrt(estVarTotalEdges))
+		fmt.Printf(", %v, estimated population mean\n", estTotalMatch/estTotalEdges)
+
+		estMeanMatch := estPopMean(estTotalMatch, estN)
+		estMeanEdges := estPopMean(estTotalEdges, estN)
+		fmt.Printf("\n")
+		fmt.Printf(", %v, est. mean matches\n", estMeanMatch)
+		fmt.Printf(", %v, est. mean edges\n", estMeanEdges)
+		fmt.Printf(", %v, est. cover\n", estMeanMatch/estMeanEdges)
+
+		varMeanMatch := estVarMean(estN, estMeanMatch, pis, jpis, matches)
+		varMeanEdges := estVarMean(estN, estMeanEdges, pis, jpis, sgEdges)
+		stdMeanMatch := math.Sqrt(varMeanMatch)
+		stdMeanEdges := math.Sqrt(varMeanEdges)
+		fmt.Printf("\n")
+		fmt.Printf(", %v, var. mean matches\n", varMeanMatch)
+		fmt.Printf(", %v, var. mean edges\n", varMeanEdges)
+		fmt.Printf(", %v, std. mean matches\n", stdMeanMatch)
+		fmt.Printf(", %v, std. mean edges\n", stdMeanEdges)
+
+		t := t_alpha_05[samples-1]
+		fmt.Printf("\n")
+		fmt.Printf(", %v - %v, interval. mean matches\n",
+			estMeanMatch - t*stdMeanMatch,
+			estMeanMatch + t*stdMeanMatch)
+		fmt.Printf(", %v - %v, interval. mean edges\n",
+			estMeanEdges - t*stdMeanEdges,
+			estMeanEdges + t*stdMeanEdges)
+		fmt.Printf(", %v - %v, interval. cover\n",
+			math.Max((estMeanMatch - t*stdMeanMatch)/(estMeanEdges + t*stdMeanEdges), 0.0),
+			math.Min((estMeanMatch + t*stdMeanMatch)/(estMeanEdges - t*stdMeanEdges), 1.0))
+	}
 	return 0
 }
 
-func estPopMean(n int, prs, ys []float64) (estMu, estVarMu float64) {
-	if len(prs) != len(ys) {
-		panic("len(prs) != len(ys)")
+func sum(xs []float64) float64 {
+	s := 0.0
+	for _, x := range xs {
+		s += x
 	}
-	pis := samplingPrs(n, prs)
+	return s
+}
+
+func estPopSize(pis []float64) float64 {
 	estN := 0.0
 	for _, pi := range pis {
 		estN += 1.0/pi
 	}
-	estT := 0.0
+	return estN
+}
+
+func estPopTotal(pis, ys []float64) (estTau float64) {
 	for i := range pis {
-		estT += ys[i]/pis[i]
+		estTau += ys[i]/pis[i]
 	}
-	estMu = estT/estN
-	estVarMu = estVarMean(n, estN, estMu, prs, pis, ys)
-	return estMu, estVarMu
+	return estTau
+}
+
+func estPopMean(estTau, estN float64) (estMu float64) {
+	estMu = estTau/estN
+	return estMu
 }
 
 func samplingPrs(n int, prs []float64) []float64 {
@@ -265,8 +363,21 @@ func jointSamplingPrs(n int, prs, pis []float64) [][]float64 {
 	return jpis
 }
 
-func estVarMean(n int, estN, estMean float64, prs, pis, ys []float64) float64 {
-	jpis := jointSamplingPrs(n, prs, pis)
+func estVarTotal(pis []float64, jpis [][]float64, ys []float64) float64 {
+	a := 0.0
+	for i, pi := range pis {
+		a += ((1 - pi)/math.Pow(pi, 2)) * math.Pow(ys[i], 2)
+	}
+	b := 0.0
+	for i := range pis {
+		for j := i + 1; j < len(pis); j++ {
+			b = ((1/(pis[i]*pis[j])) - (1/jpis[i][j])) * ys[i] * ys[j]
+		}
+	}
+	return a + 2*b
+}
+
+func estVarMean(estN, estMean float64, pis []float64, jpis [][]float64, ys []float64) float64 {
 	a := 0.0
 	for _, pi := range pis {
 		a += (1 - pi)/(math.Pow(pi, 2))
